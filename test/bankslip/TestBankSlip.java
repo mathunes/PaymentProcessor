869c6b835fb34372e9d89816395cfc41fa6e8bac
Mathunes <matheusantunes720@gmail.com>
2022-04-12 15:05:06 -0300

    add check bank slip data test

--- test/bankslip/TestBankSlip.java
+++ test/bankslip/TestBankSlip.java
@@ -1,5 +1,9 @@
 package bankslip;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.Date;
 
 import org.junit.Before;
@@ -13,6 +17,13 @@ public class TestBankSlip {
 	@Before
 	public void setUp() {
 		bankSlip = new BankSlip("9F86D081884C7D659A2FEAA0C55AD0", new Date(), 50.00);
+	}
+	
+	@Test
+	public void testCheckBankSlipData() {
+		assertEquals(bankSlip.getCode(), "9F86D081884C7D659A2FEAA0C55AD0");
+		assertTrue(bankSlip.getDate() instanceof Date);
+		assertEquals(bankSlip.getValue(), 50.00);
 	}
 	
 }

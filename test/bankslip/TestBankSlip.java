4ae432cde244830b4b3c443537d69f2fe3d67544
Mathunes <matheusantunes720@gmail.com>
2022-04-12 15:02:10 -0300

    add initialize bank slip test

--- test/bankslip/TestBankSlip.java
+++ test/bankslip/TestBankSlip.java
@@ -1,5 +1,18 @@
 package bankslip;
 
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("Bank slip test class")
 public class TestBankSlip {
 
+	private BankSlip bankSlip;
+	
+	@Before
+	public void setUp() {
+		bankSlip = new BankSlip("9F86D081884C7D659A2FEAA0C55AD0", new Date(), 50.00);
+	}
+	
 }

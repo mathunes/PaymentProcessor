95029b1929d0e42b59e8b0d5f3e6fb40fc6f7590
Mathunes <matheusantunes720@gmail.com>
2022-04-12 14:51:26 -0300

    develop Invoice class

---
+++ src/invoice/Invoice.java
@@ -0,0 +1,55 @@
+package invoice;
+
+import java.util.Date;
+
+public class Invoice {
+	
+	public Invoice(Date date, double total, String clientName) {
+		super();
+		setDate(date);
+		setTotal(total);
+		setClientName(clientName);
+		setPaid(false);
+	}
+
+	public Invoice() {
+	}
+
+	private Date date;
+	private double total;
+	private String clientName;
+	private boolean paid;
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public double getTotal() {
+		return total;
+	}
+	
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	
+	public String getClientName() {
+		return clientName;
+	}
+	
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
+
+	public boolean isPaid() {
+		return paid;
+	}
+
+	public void setPaid(boolean paid) {
+		this.paid = paid;
+	}
+	
+}

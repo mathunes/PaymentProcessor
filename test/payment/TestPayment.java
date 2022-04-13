4537f7b46da49144c0aa5ded5fd1299df919a53d
Mathunes <matheusantunes720@gmail.com>
2022-04-13 11:31:46 -0300

    add check payment data test

--- test/payment/TestPayment.java
+++ test/payment/TestPayment.java
@@ -1,8 +1,12 @@
 package payment;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.Date;
 
 import org.junit.Before;
+import org.junit.Test;
 
 public class TestPayment {
 
@@ -11,6 +15,13 @@ public class TestPayment {
 	@Before
 	public void setUp() {
 		payment = new Payment(20.00, new Date(), "BANK_SLIP");
+	}
+	
+	@Test
+	public void testCheckPaymentData() {
+		assertEquals(payment.getValue(), 20.00);
+		assertTrue(payment.getDate() instanceof Date);
+		assertEquals(payment.getType(), "BANK_SLIP");
 	}
 	
 }

cd45776c7299af4975ea5d363b2c4226a0ff6b46
Mathunes <matheusantunes720@gmail.com>
2022-04-13 09:36:23 -0300

    add initialize processor test

---
+++ test/processor/TestProcessor.java
@@ -0,0 +1,14 @@
+package processor;
+
+import org.junit.Before;
+
+public class TestProcessor {
+
+	private Processor processor;
+	
+	@Before
+	public void setUp() {
+		processor = new Processor();
+	}
+
+}

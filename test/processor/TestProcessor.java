8a626192cd9a25dd25ffeb2e94b92039e86b7f06
Mathunes <matheusantunes720@gmail.com>
2022-04-13 10:20:38 -0300

    add testPayTheSameValueOfInvoice

--- test/processor/TestProcessor.java
+++ test/processor/TestProcessor.java
@@ -1,6 +1,13 @@
 package processor;
 
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+
+import bankslip.BankSlip;
+import invoice.Invoice;
 
 public class TestProcessor {
 
@@ -9,6 +16,23 @@ public class TestProcessor {
 	@BeforeEach
 	public void setUp() {
 		processor = new Processor();
+	}
+	
+	@Test
+	public void testPayTheSameValueOfInvoice() {
+		Invoice invoice = new Invoice(new Date(), 100.00, "Julie");
+		
+		BankSlip bankSlip1 = new BankSlip("9F86D081884C7D659A2FEAA0C55AD0", new Date(), 30.00);
+		BankSlip bankSlip2 = new BankSlip("DAD080581984A0D6FEAA26C559C7F8", new Date(), 50.00);
+		BankSlip bankSlip3 = new BankSlip("A0DC559C07D6F88FEADA8A26094581", new Date(), 20.00);
+		
+		List<BankSlip> bankSlips;
+		
+		bankSlips.add(bankSlip1);
+		bankSlips.add(bankSlip2);
+		bankSlips.add(bankSlip3);
+		
+		processor.process(invoice, bankSlips);
 	}
 
 }

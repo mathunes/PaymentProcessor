8b75d252fdcb05b9396946f31c306c3473f35ba8
Mathunes <matheusantunes720@gmail.com>
2022-04-13 11:30:01 -0300

    develop Payment class

--- src/payment/Payment.java
+++ src/payment/Payment.java
@@ -1,5 +1,41 @@
 package payment;
 
-public class Payment {
+import java.util.Date;
 
+public class Payment {
+	
+	private double value;
+	private Date date;
+	private String type;
+	
+	public Payment(double value, Date date, String type) {
+		this.value = value;
+		this.date = date;
+		this.type = type;
+	}
+
+	public double getValue() {
+		return value;
+	}
+	
+	public void setValue(double value) {
+		this.value = value;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
+	
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
 }

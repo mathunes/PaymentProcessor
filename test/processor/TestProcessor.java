7db680f7de2fb96e0f55b6ef6d87e063ee5bb0c0
Mathunes <matheusantunes720@gmail.com>
2022-04-13 11:54:08 -0300

    add test pay more than the value of invoice

--- test/processor/TestProcessor.java
+++ test/processor/TestProcessor.java
@@ -79,5 +79,24 @@ public class TestProcessor {
 		
 		assertFalse(invoice.isPaid());
 	}
+
+	@Test
+	public void testPayMoreThanTheValueOfInvoice() {
+		Invoice invoice = new Invoice(new Date(), 100.00, "Julie");
+		
+		BankSlip bankSlip1 = new BankSlip("9F86D081884C7D659A2FEAA0C55AD0", new Date(), 30.00);
+		BankSlip bankSlip2 = new BankSlip("DAD080581984A0D6FEAA26C559C7F8", new Date(), 50.00);
+		BankSlip bankSlip3 = new BankSlip("A0DC559C07D6F88FEADA8A26094581", new Date(), 50.00);
+		
+		List<BankSlip> bankSlips = new ArrayList<BankSlip>();
+		
+		bankSlips.add(bankSlip1);
+		bankSlips.add(bankSlip2);
+		bankSlips.add(bankSlip3);
+		
+		processor.process(invoice, bankSlips);
+		
+		assertTrue(invoice.isPaid());
+	}
 	
 }

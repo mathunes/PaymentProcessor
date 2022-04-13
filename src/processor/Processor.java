75a62312905ff85a9306ecd1bfc6182de695a4df
Mathunes <matheusantunes720@gmail.com>
2022-04-13 10:17:23 -0300

    remove Processor attributes

--- src/processor/Processor.java
+++ src/processor/Processor.java
@@ -7,28 +7,8 @@ import invoice.Invoice;
 
 public class Processor {
 
-	private Invoice invoice;
-	private List<BankSlip> bankSlips;
-	
-	public Processor(Invoice invoice, List<BankSlip> bankSlips) {
-		this.invoice = invoice;
-		this.bankSlips = bankSlips;
-	}
+	public Processor() {
 
-	public Invoice getInvoice() {
-		return invoice;
-	}
-	
-	public void setInvoice(Invoice invoice) {
-		this.invoice = invoice;
-	}
-	
-	public List<BankSlip> getBankSlips() {
-		return bankSlips;
-	}
-	
-	public void setBankSlips(List<BankSlip> bankSlips) {
-		this.bankSlips = bankSlips;
 	}
 	
 }
